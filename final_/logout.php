8b045e51c1e544fbd8ba53b703b66742d77b828c
RipunjayChoudhury <155419058+RipunjayChoudhury@users.noreply.github.com>
2024-12-29 21:55:20 +0530

    Delete final_ directory

--- final_/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-session_unset();
-session_destroy();
-echo "<script>alert('You have been logged out successfully.');</script>";
-echo "<script>window.location.href='home.php';</script>";
-exit();
-?>
